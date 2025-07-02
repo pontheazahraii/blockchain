0546bfbbf1c4a3e43940af14df86ed9d7ea5e9b6
Ponthea Zahraii <pontheazahraii@gmail.com>
2025-07-01 23:50:28 -0700

    implement basic proof of work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -49,6 +51,11 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    @property
+    def last_block(self):
+        """Returns the last Block in the chain"""
+        return self.chain[-1]
+
     @staticmethod
     def hash(block):
         """Creates a SHA-256 hash of a Block
@@ -62,7 +69,31 @@ class Blockchain(object):
         block_string = json.dump(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    @property
-    def last_block(self):
-        """Returns the last Block in the chain"""
-        return self.chain[-1]
+    def proof_of_work(self, last_proof):
+        """Simple Proof of Work Algorithm:
+        - Find a number p' such that hash(pp') contains leading 4 zeroes,
+        where p is the previous p'
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof): 
+        """Validates the Proof: does hash(last_proof, proof)
+        contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
