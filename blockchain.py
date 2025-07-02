76782f4110da5de80bbc07719587f4fa143355db
Ponthea Zahraii <pontheazahraii@gmail.com>
2025-07-01 23:24:31 -0700

    add new_transaction()

--- blockchain.py
+++ blockchain.py
@@ -7,16 +7,28 @@ class Blockchain(object):
         """Creates a new Block and adds it to the chain"""
         ...
 
-    def new_transaction(self):
-        """Adds a new transaction to the list of transactions"""
-        ...
+    def new_transaction(self, sender, recipient, amount):
+        """Adds a new transaction to the list of transactions
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transaction.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
         """Hashes a Block
 
-        :param block: the block to hash
-        :type block: Blockchain
+        :param block: <Blockchain> the block to hash
         """
         ...
 
